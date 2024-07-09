bf52553d5af2fe99aa618a7283a90f7ead21fbf8
harshit_garg <114936100+Harshit-880@users.noreply.github.com>
2024-07-09 21:22:19 +0530

    Update urls.py

--- healthcare/urls.py
+++ healthcare/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from account.views import signup_view, login_view, dashboard_view,logout_view,home_view
 
 urlpatterns = [
-    path('', admin.site.urls),
-    path('', include('account.urls')),
+    path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+    path('logout/', logout_view, name='logout'),
+    path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
+    path('dashboard/', dashboard_view, name='dashboard'),
 ]
 
 if settings.DEBUG:
