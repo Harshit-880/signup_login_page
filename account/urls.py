eec9096f64e6fb3fb589ed8883773b608b19c563
harshit_garg <114936100+Harshit-880@users.noreply.github.com>
2024-07-09 21:09:16 +0530

    Update urls.py

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import signup_view, login_view, dashboard_view,logout_view
+from .views import signup_view, login_view, dashboard_view,logout_view,home_view
 
 urlpatterns = [
     path('', home_view, name='home'),
